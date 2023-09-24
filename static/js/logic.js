9c464f8e3fd499a007d7323067d50db4709c3b78
captainchicken2023 <captainchicken2023@gmail.com>
2023-09-24 13:24:14 -0500

    update

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,63 +1,131 @@
-// Creating the map object
-let myMap = L.map("map", {
-    center: [31.7917, 7.0926],
-    zoom: 2
+// Store our API endpoint as queryUrl.
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
+
+// Perform a GET request to the query URL/
+d3.json(queryUrl).then(function (data) {
+  // Once we get a response, send the data.features object to the createFeatures function.
+  createFeatures(data.features);
+});
+
+function createFeatures(earthquakeData) {
+
+  // Define a function that we want to run once for each feature in the features array.
+  // Give each feature a popup that describes the place and time of the earthquake.
+  function onEachFeature(feature, layer) {
+    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+  }
+
+  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+  // Run the onEachFeature function once for each piece of data in the array.
+  let earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature
   });
-  
-  // Adding the tile layer
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+
+  // Send our earthquakes layer to the createMap function/
+  createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+    // https://leaflet-extras.github.io/leaflet-providers/preview/
+  // Create the base layers.
+  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  })
+
+  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+  });
+
+  // Create a baseMaps object.
+  let baseMaps = {
+    "Street Map": street,
+    "Topographic Map": topo
+  };
+
+  // Create an overlay object to hold our overlay.
+  let overlayMaps = {
+    Earthquakes: earthquakes
+  };
+
+  // Create our map, giving it the streetmap and earthquakes layers to display on load.
+  let myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [street, earthquakes]
+  });
+
+  // Create a layer control.
+  // Pass it our baseMaps and overlayMaps.
+  // Add the layer control to the map.
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
   }).addTo(myMap);
-  
-  // Store the API query variables.
-  let URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
 
-  // Get the data with d3.
-  d3.json(url).then(function(response) {
-  
-    // Create a new marker cluster group.
-    let markers = L.markerClusterGroup();
-  
-    // Loop through the data.
-    for (let i = 0; i < response.length; i++) {
-  
-      // Set the data location property to a variable.
-      let location = response[i].location;
-  
-      // Check for the location property.
-      if (location) {
-  
-        // Add a new marker to the cluster group, and bind a popup.
-        markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
-          .bindPopup(response[i].descriptor));
-      }
-  
-    }
-  
-    // Add our marker cluster layer to the map.
-    myMap.addLayer(markers);
-  
-  });
-  
-
-  //marker.bindPopup("insert message here");
-  //Stylize markers based on earthquake severity
+}
 
 
-//   // Define a markerSize() function that will give each city a different radius based on its population.
-// function markerSize(population) {
-//     return Math.sqrt(population) * 50;
-//   }
+// // Creating the map object
+// let myMap = L.map("map", {
+//     center: [31.7917, 7.0926],
+//     zoom: 2
+//   });
   
-//   // Loop through the cities array, and create one marker for each city object.
-//   for (let i = 0; i < cities.length; i++) {
-//     L.circle(cities[i].location, {
-//       fillOpacity: 0.75,
-//       color: "white",
-//       fillColor: "purple",
-//       // Setting our circle's radius to equal the output of our markerSize() function:
-//       // This will make our marker's size proportionate to its population.
-//       radius: markerSize(cities[i].population)
-//     }).bindPopup(`<h1>${cities[i].name}</h1> <hr> <h3>Population: ${cities[i].population.toLocaleString()}</h3>`).addTo(myMap);
-//   }
+//   // Adding the tile layer
+//   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//   }).addTo(myMap);
+  
+//   // Store the API query variables.
+//   let URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
+
+//   // Get the data with d3.
+//   d3.json(url).then(function(response) {
+  
+//     // Create a new marker cluster group.
+//     let markers = L.markerClusterGroup();
+  
+//     // Loop through the data.
+//     for (let i = 0; i < response.length; i++) {
+  
+//       // Set the data location property to a variable.
+//       let location = response[i].location;
+  
+//       // Check for the location property.
+//       if (location) {
+  
+//         // Add a new marker to the cluster group, and bind a popup.
+//         markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
+//           .bindPopup(response[i].descriptor));
+//       }
+  
+//     }
+  
+//     // Add our marker cluster layer to the map.
+//     myMap.addLayer(markers);
+  
+//   });
+  
+
+//   //marker.bindPopup("insert message here");
+//   //Stylize markers based on earthquake severity
+
+
+// //   // Define a markerSize() function that will give each city a different radius based on its population.
+// // function markerSize(population) {
+// //     return Math.sqrt(population) * 50;
+// //   }
+  
+// //   // Loop through the cities array, and create one marker for each city object.
+// //   for (let i = 0; i < cities.length; i++) {
+// //     L.circle(cities[i].location, {
+// //       fillOpacity: 0.75,
+// //       color: "white",
+// //       fillColor: "purple",
+// //       // Setting our circle's radius to equal the output of our markerSize() function:
+// //       // This will make our marker's size proportionate to its population.
+// //       radius: markerSize(cities[i].population)
+// //     }).bindPopup(`<h1>${cities[i].name}</h1> <hr> <h3>Population: ${cities[i].population.toLocaleString()}</h3>`).addTo(myMap);
+// //   }
   
